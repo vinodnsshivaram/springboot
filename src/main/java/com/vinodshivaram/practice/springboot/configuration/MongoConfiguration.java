5e7d52e8f1b94ffc79ee5af9826331f68051a8f9
vinodnsshivaram <vinodnsshivaram@gmail.com>
2019-04-12 16:57:14 -0600

    code format.

--- src/main/java/com/vinodshivaram/practice/springboot/configuration/MongoConfiguration.java
+++ src/main/java/com/vinodshivaram/practice/springboot/configuration/MongoConfiguration.java
@@ -1,11 +1,18 @@
 package com.vinodshivaram.practice.springboot.configuration;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("mongodb")
 public class MongoConfiguration {
+    protected Logger logger = LoggerFactory.getLogger(MongoConfiguration.class);
     private String uri;
     private String host;
     private String port;
@@ -59,5 +66,21 @@ public class MongoConfiguration {
 
     public void setDatabase(String database) {
         this.database = database;
+    }
+
+    @Bean
+    public MongoClient getMongoClient() {
+        logger.info("Initializing Mongo Client");
+        if (uri == null)
+            throw new RuntimeException("Mongo URI is not defined in properties");
+        return new MongoClient(new MongoClientURI(uri));
+    }
+
+    @Bean
+    public MongoTemplate getMongoTemplate() {
+        logger.info("Initializing Mongo Template");
+        if (database == null)
+            throw new RuntimeException("Database name is not defined in properties");
+        return new MongoTemplate(getMongoClient(), database);
     }
 }
